e0a90a23151b0aacc78a72cc71132596c67a6dc9
Micah Hawkins <micahbhawkins@gmail.com>
2014-11-17 05:59:34 -0800

    Push to continue at work

--- js/app.js
+++ js/app.js
@@ -23,8 +23,8 @@ Enemy.prototype.render = function() {
 
 // Now write your own player class
 // This class requires an update(), render() and
-// a handleInput() method.
-
+// extend Enemy and override the sprite
+// add a handleInput() method.
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
